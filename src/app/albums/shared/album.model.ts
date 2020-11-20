070f8dd8ea909e4a554f8d81089e48dae1f1158f
Douglas Sauer <8331740+dougsauer@users.noreply.github.com>
2020-11-20 04:09:06 -0600

    Add Album model.

---
+++ src/app/albums/shared/album.model.ts
@@ -0,0 +1,54 @@
+/*  Copyright 2020 by Douglas Sauer | Blue Heart Software. */
+
+export interface AlbumCopy {
+  purchaseDate: Date;
+  purchasedFrom: string;
+  purchaseCondition: string;
+  purchasePrice: number;
+  publisher: string;
+  manufacturedIn: string;
+  releaseYear: number;
+  catalogId: string;
+  upc: string;
+  promo: boolean;
+  recordClubEdition: boolean;
+  reissue: boolean;
+  reMastered: boolean;
+  channels: number;
+  mediaType: string;
+  mediaSubtype: string;
+  halfSpeedMaster: boolean;
+  mediaCondition: string;
+  coverCondition: string;
+  coverCutOut: boolean;
+  notes: string;
+}
+
+export interface Song {
+  order: number;
+  name: string;
+  duration: string;
+}
+
+export interface Album {
+  _id: string;
+  title: string;
+  artist: string;
+  label: string;
+  genre: string;
+  style: string;
+  originalReleaseYear: number;
+  numberOfDiscs: number;
+  boxEdition: boolean;
+  gatefold: boolean;
+  compilation: boolean;
+  live: boolean;
+  soundtrack: boolean;
+  specialEdition: boolean;
+  frontCover: string;
+  backCover: string;
+  songs: Song[];
+  copies: AlbumCopy[];
+  rating: number;
+  comments: string;
+}
