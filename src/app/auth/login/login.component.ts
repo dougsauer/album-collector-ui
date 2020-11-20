410ff87d6f6b76b87ab59c92014ae1d86964456b
Douglas Sauer <8331740+dougsauer@users.noreply.github.com>
2020-11-20 04:13:03 -0600

    Shell login component

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,12 @@
+/*  Copyright 2020 by Douglas Sauer | Blue Heart Software. */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+}
