46bcb4494a34343bfa890ab5911e84ed71be4148
Douglas Sauer <8331740+dougsauer@users.noreply.github.com>
2020-11-20 04:14:11 -0600

    Initial album list view

---
+++ src/app/albums/album-list/album-list.component.ts
@@ -0,0 +1,10 @@
+/*  Copyright 2020 by Douglas Sauer | Blue Heart Software. */
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-album-list',
+  templateUrl: './album-list.component.html',
+  styleUrls: ['./album-list.component.scss']
+})
+export class AlbumListComponent {
+}
