717d41bfa017a73a47768e7f9f9b33ca6a214ce8
Douglas Sauer <8331740+dougsauer@users.noreply.github.com>
2020-11-20 04:14:47 -0600

    Add router view and change title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 /*  Copyright 2020 by Douglas Sauer | Blue Heart Software. */
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'album-collector-ui';
+  title = 'Album Collector';
 }
